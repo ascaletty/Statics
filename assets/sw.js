2d77d096820903609d88babb8aa9823f6ae7a0f5
Aaron Scaletty <ascaletty23@gmail.com>
2026-03-14 15:20:25 -0500

    Trunk integration

---
+++ assets/sw.js
@@ -0,0 +1,23 @@
+var cacheName = 'egui-template-pwa';
+var filesToCache = [
+  './index.html',
+  '../target/wasm32-unknown-unknown/debug/truss.wasm',
+];
+
+/* Start the service worker and cache all of the app's content */
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function (cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+/* Serve cached content when offline */
+self.addEventListener('fetch', function (e) {
+  e.respondWith(
+    caches.match(e.request).then(function (response) {
+      return response || fetch(e.request);
+    })
+  );
+});
